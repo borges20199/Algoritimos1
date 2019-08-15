c87adfcff3cc9a316586609cbf376882e28d61b2
borges20199 <47629605+borges20199@users.noreply.github.com>
2019-08-14 22:07:29 -0300

    lista de exercícios com parâmetros

--- aula.py
+++ aula.py
@@ -1,103 +1,56 @@
-# def pedeMostra():
-#     x = int(input(" Digite um número "))
-#     print('o número digitado foi', x)
-# pedeMostra()
-
-# def exibirMenu():
-#     print("""
-#     ****Menu Principal****         
-#         1. incluir
-#         2. alterar
-#         3. excluir 
-#         4. sair 
-#     """)
-# exibirMenu()
-
-
-######lista de exercicos#######
-# ex 1)
-# a)
-
-def somar():
-    x = int(input(" Digite o primeiro número: "))
-    y = int(input(" Digite o segundo número: "))
-    soma = (x + y)
-    print(x, "+", y, "=", soma)
-# somar()
-
-# b)
-
-
-def multiplicar():
-    x = int(input(" Digite o primeiro número: "))
-    y = int(input(" Digite o segundo número: "))
-    mlt = (x * y)
-    print(x, "*", y, "=",  mlt)
-# multiplicar()
-
-
-#c)
-
-import math
-def raiz():
-    x = int(input(" Digite um número: "))
-    y = math.sqrt(x)
-    print(" A raiz quadrada de ", x, "é ", y)
-# raiz()
-
-#d)
-
-
-def potencia():
-    x = int(input(" Digite um número: "))
-    y = int(input(" Digite um número: "))
-    z = math.pow(x,y)
-    print(x, "elevado a ", y, "é igual a ", Z)
-# potencia()
-
-#e)
-
-def tabuada():
-    x = int(input(" Digite um número: "))
-    cont = 1
-    while(cont <= 10 ):
-        multi = cont * x
-        print(cont,"*", x, "=", multi)
-        cont = cont + 1
-# tabuada()
-
-#2)
-def menuOperacoes():
-    print("""
-    Menu Principal 
-    1.Soma
-    2.Multiplicação
-    3.Raiz
-    4.Potência
-    5.Tabuada
-    6.Sair
-    """)
+# def somar (a, b, c):
+#     soma = a + b + c
+#     print(soma)
+# somar(10, 20, 30)
+######Lista 2 com parâmetros#######
+#ex1-
+#A)
+# def somar(a, b):
+#     soma = a + b  
+#     print(" o resultado é ", soma )
 
 
 
 
+# a = int(input(" Digite o 1. valor: "))
+# b = int(input(" Digite o 2. valor: "))
+# somar(a, b)
 
-while(True):    
-    menuOperacoes()
-    opcao = int(input("Escolha a opção que deseja realizar:"))
-    if(opcao == 1):
-        somar()
-    elif(opcao == 2):
-        multiplicar()
-    elif(opcao == 3):
-        raiz()
-    elif(opcao == 4):
-        potencia()
-    elif(opcao == 5):
-        tabuada()
-    elif(opcao == 6):
-        break
-    else:
-        print(" Opção inválida ! Por favior escoha uma opção existente ")
-        
+#B)
+# def multiplicar(a, b):
+#     mlt = a * b
+#     print(" o resultado é ", mlt)
 
+
+# a = int(input(" Digite o 1. valor: "))
+# b = int(input(" Digite o 2. valor: "))
+# multiplicar(a, b)
+
+#C)
+# import math
+# def raiz(a):
+#     r = math.sqrt(a)
+#     print(" o resultado é ", r)
+
+
+# a = int(input(" Digite o valor: "))
+# raiz(a)
+
+#D)
+# def potencia(a, b):
+#     p = math.pow(a, b)
+#     print(" o resultado é ", p)
+
+# a = int(input(" Digite o valor: "))
+# b = int(input(" Digite o valor: "))
+# potencia(a, b)
+
+#E)
+# def tabuada(x):
+#     cont = 1
+#     while(cont <= 10):
+#         multi = cont * x
+#         print(cont, "*", x, "=", multi)
+#         cont = cont + 1
+# x = int(input(" Digite um número: "))
+# tabuada(x)
