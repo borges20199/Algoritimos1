394ccfc9b5168d617fcc862c7d83ebc37196a4ec
borges20199 <47629605+borges20199@users.noreply.github.com>
2019-09-18 21:45:08 -0300

    exeplo - número maior 18/09/2019

--- aula.py
+++ aula.py
@@ -1,56 +1,9 @@
-# def somar (a, b, c):
-#     soma = a + b + c
-#     print(soma)
-# somar(10, 20, 30)
-######Lista 2 com parâmetros#######
-#ex1-
-#A)
-# def somar(a, b):
-#     soma = a + b  
-#     print(" o resultado é ", soma )
+import principal
 
 
 
 
-# a = int(input(" Digite o 1. valor: "))
-# b = int(input(" Digite o 2. valor: "))
-# somar(a, b)
-
-#B)
-# def multiplicar(a, b):
-#     mlt = a * b
-#     print(" o resultado é ", mlt)
-
-
-# a = int(input(" Digite o 1. valor: "))
-# b = int(input(" Digite o 2. valor: "))
-# multiplicar(a, b)
-
-#C)
-# import math
-# def raiz(a):
-#     r = math.sqrt(a)
-#     print(" o resultado é ", r)
-
-
-# a = int(input(" Digite o valor: "))
-# raiz(a)
-
-#D)
-# def potencia(a, b):
-#     p = math.pow(a, b)
-#     print(" o resultado é ", p)
-
-# a = int(input(" Digite o valor: "))
-# b = int(input(" Digite o valor: "))
-# potencia(a, b)
-
-#E)
-# def tabuada(x):
-#     cont = 1
-#     while(cont <= 10):
-#         multi = cont * x
-#         print(cont, "*", x, "=", multi)
-#         cont = cont + 1
-# x = int(input(" Digite um número: "))
-# tabuada(x)
+num1 = int(input(" Digite o número: "))
+num2 = int(input(" Digite o número: "))
+k = principal.maior(num1, num2)
+print("O maior número é o ", k)
---
+++ principal.py
@@ -0,0 +1,8 @@
+def maior(a,b):
+    if(a > b):
+        return a
+    elif(a < b):
+        return b
+    else:
+        print(" os números são iguais !!")
+        return a
